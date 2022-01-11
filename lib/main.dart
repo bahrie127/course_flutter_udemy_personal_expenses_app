22ab36bd14913ba3d7583e95f127ce296ec6ab24
bahri <bahri@atmatech.net>
2022-01-11 15:37:01 +0700

    Fetching user input

--- lib/main.dart
+++ lib/main.dart
@@ -33,6 +33,9 @@ class MyHomePage extends StatelessWidget {
     ),
   ];
 
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -59,17 +62,18 @@ class MyHomePage extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
                   TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Title',
-                    ),
+                    decoration: InputDecoration(labelText: 'Title'),
+                    controller: titleController,
                   ),
                   TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Amount',
-                    ),
+                    decoration: InputDecoration(labelText: 'Amount'),
+                    controller: amountController,
                   ),
                   TextButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      print(titleController.text);
+                      print(amountController.text);
+                    },
                     child: Text('Add Transaction'),
                     style: TextButton.styleFrom(
                       primary: Colors.purple,
